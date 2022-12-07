c01feb33403771a3f1794ab085a1c2c89b185457
Luis Gabriel Hernández Reséndiz <120036603@upq.edu.mx>
2022-12-06 20:19:42 -0600

    Añadi CRUP, nuevas vistas editar y comfirmar eliminacion y añadi columnas que me faltaron para consulta

--- Mi_Cyber/resources/views/consulta.blade.php
+++ Mi_Cyber/resources/views/consulta.blade.php
@@ -2,14 +2,14 @@
 @section('contenido')
 <h1 class = "mt-4 display-1 text-center text-light"> <i class="bi bi-card-checklist"></i> Consulta <h1>
 <body style="background-color: #8b0000">
-<body style="background-color: # ">
-<div class="container col-md-6">
+<div class="container col-md-8">
     <table class="table table-dark table-borderless">
     <tr>
     <th scope="col">ID</th>
     <th scope="col">Usuario</th>
     <th scope="col">No. Compu</th>
     <th scope="col">Tiempo</th>
+    <th scope="col">Fecha</th>
     <th scope="col">Acciones</th>
     </tr>
     @foreach ($consultarCompu as $buscarComputadora)
@@ -17,6 +17,7 @@
     <th>{{ $buscarComputadora->idC }}</th>
     <td>{{ $buscarComputadora->usuario }}</td>
     <td>{{ $buscarComputadora->Ncompu }}</td>
+    <td>{{ $buscarComputadora->tiempo }}</td>
     <td>{{ $buscarComputadora->fecha }}</td>
     <td>
         <a href="{{ route('listaCompu.edit', $buscarComputadora->idC) }}" class="btn btn-outline-warning">Editar <i class="bi bi-pencil"></i></a>
